8f25dacb1fcef687e1099294a13ec7b6df8fd28b
headblockhead <26520767+headblockhead@users.noreply.github.com>
2020-11-16 12:31:11 +0000

    listed primes

--- runmaininloop.py
+++ runmaininloop.py
@@ -3,7 +3,9 @@ import sys
 
 val = 0
 num = sys.argv[1]
-os.system('rm ./output.txt')
+os.system('rm -rfvd ./output.txt')
+os.system('rm -rfvd ./notprimes.txt')
+os.system('rm -rfvd ./primes.txt')
 
 while val < int(num) :
 	val = val + 1
